9c48651b006b91506c54aaa29a22bd61c4cfe3c2
jhonelrios <riosjaimes.j@gmail.com>
2024-04-14 20:26:42 -0500

    Initial Commit

---
+++ natuar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  natuar
+//
+//  Created by Jhonel Rios Jaimes on 14/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ natuar/natuarApp.swift
@@ -0,0 +1,17 @@
+//
+//  natuarApp.swift
+//  natuar
+//
+//  Created by Jhonel Rios Jaimes on 14/04/24.
+//
+
+import SwiftUI
+
+@main
+struct natuarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
