e23a1ceba37787da5433e87033fc036d6dbfd33f
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2024-09-02 18:35:14 -0500

    core: offline files structure

---
+++ natuar/Domains/Shared/Data/AnimalsData.swift
@@ -0,0 +1,12 @@
+//
+//  AnimalsData.swift
+//  natuar
+//
+//  Created by Jhonel Rios Jaimes on 2/09/24.
+//
+
+import Foundation
+
+let animalsData: [Animal] = [
+    Animal(id: 10, name: "Guacamayo Rojo", scientific_name: "Ara macao", description: <#T##String#>, weigth: <#T##Int#>, height: <#T##Int#>, average_age: <#T##Int#>, habitat: <#T##String#>, diet: <#T##String#>, gestation: <#T##String#>, in_danger: <#T##Bool#>, images: <#T##[String]#>, model_name: <#T##String#>, latitude: <#T##Double#>, longitude: <#T##Double#>, spotId: <#T##Int#>)
+]
