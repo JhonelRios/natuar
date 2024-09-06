25b60f42dd494f2b865750919c82a7786149ad68
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2024-09-06 17:09:55 -0500

    feat: animal view model refactor

--- natuar/Domains/Animal/ViewModel/AnimalViewModel.swift
+++ natuar/Domains/Animal/ViewModel/AnimalViewModel.swift
@@ -19,230 +19,40 @@ class AnimalViewModel : ObservableObject {
     @Published var isFavorite = false
     
     func fetchAnimals(completion: @escaping (Bool) -> Void) {
-        let animalsUrl = "\(Constants.backendURL)/animals"
-        guard let url = URL(string: animalsUrl) else {
-            errorMessage = "Invalid URL"
-            return
-        }
-        
-        let accessToken = UserDefaultsManager().getAccessToken()
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization") // Add your token
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        isLoading = true
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-                self.isLoading = false
-                
-                if let error = error {
-                    self.errorMessage = "Animals request failed: \(error.localizedDescription)"
-                    completion(false)
-                    return
-                }
-                
-                guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                    self.errorMessage = "Error: Non-200 HTTP status code"
-                    completion(false)
-                    return
-                }
-                
-                if let animalsResponse = try? JSONDecoder().decode([Animal].self, from: data) {
-                    self.animals = animalsResponse
-                    completion(true)
-                } else {
-                    self.errorMessage = "Failed to decode animals response"
-                    completion(false)
-                }
-            }
-        }.resume()
+        self.animals = animalsData
+        completion(true)
     }
     
     func fetchIsFavorite(animalId: Int, completion: @escaping (Bool) -> Void) {
-        let isFavoriteUrl = "\(Constants.backendURL)/animals/\(animalId)/favorite"
-        guard let url = URL(string: isFavoriteUrl) else {
-            errorMessage = "Invalid URL"
-            return
+        self.isFavorite = offlineUser.favoriteAnimals.contains { animal in
+            animal.id == animalId
         }
-        
-        let accessToken = UserDefaultsManager().getAccessToken()
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization") // Add your token
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        isLoading = true
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-                self.isLoading = false
-                
-                if let error = error {
-                    self.errorMessage = "Is Favorite request failed: \(error.localizedDescription)"
-                    completion(false)
-                    return
-                }
-                
-                guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                    self.errorMessage = "Error: Non-200 HTTP status code"
-                    completion(false)
-                    return
-                }
-                
-                if let isFavoriteResponse = try? JSONDecoder().decode(isFavoriteResponse.self, from: data) {
-                    self.isFavorite = isFavoriteResponse.isFavorite
-                    completion(true)
-                } else {
-                    self.errorMessage = "Failed to decode is favorite response"
-                    completion(false)
-                }
-            }
-        }.resume()
+        completion(true)
     }
     
     func markAsFavorite(animalId: Int, completion: @escaping (Bool) -> Void) {
-        let isFavoriteUrl = "\(Constants.backendURL)/animals/\(animalId)/favorite"
-        guard let url = URL(string: isFavoriteUrl) else {
-            errorMessage = "Invalid URL"
+        guard let selectedAnimal = animalsData.first(where: { animal in animal.id == animalId }) else {
+            print("No animal with the ID: \(animalId)")
             return
         }
         
-        let accessToken = UserDefaultsManager().getAccessToken()
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization") // Add your token
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-//        isLoading = true
-        isFavorite = true
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-//                self.isLoading = false
-                
-                if let error = error {
-                    self.errorMessage = "Mark as Favorite request failed: \(error.localizedDescription)"
-                    self.isFavorite = false
-                    completion(false)
-                    return
-                }
-                
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    self.errorMessage = "Invalid response from the server"
-                    self.isFavorite = false
-                    completion(false)
-                    return
-                }
-                
-                switch httpResponse.statusCode {
-                case 201:
-                    completion(true)
-                default:
-                    self.errorMessage = "Failed with HTTP code: \(httpResponse.statusCode)"
-                    self.isFavorite = false
-                    completion(false)
-                }
-            }
-        }.resume()
+        offlineUser.favoriteAnimals.append(selectedAnimal)
+        completion(true)
     }
     
     func deleteFavoriteAnimals(animalId: Int, completion: @escaping (Bool) -> Void) {
-        let favoritesUrl = "\(Constants.backendURL)/animals/favorite/\(animalId)"
-        guard let url = URL(string: favoritesUrl) else {
-            errorMessage = "Invalid URL"
-            return
+        offlineUser.favoriteAnimals = offlineUser.favoriteAnimals.filter {
+            $0.id != animalId
         }
-        
-        let accessToken = UserDefaultsManager().getAccessToken()
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "DELETE"
-        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization") // Add your token
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-//        isLoading = true
-        isFavorite = false
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-//                self.isLoading = false
-                
-                if let error = error {
-                    self.errorMessage = "Delete request failed: \(error.localizedDescription)"
-                    completion(false)
-                    self.isFavorite = true
-                    return
-                }
-                
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    self.errorMessage = "Invalid response from the server"
-                    completion(false)
-                    self.isFavorite = true
-                    return
-                }
-                
-                switch httpResponse.statusCode {
-                case 200:
-                    completion(true)
-                default:
-                    self.errorMessage = "Failed with HTTP code: \(httpResponse.statusCode)"
-                    completion(false)
-                    self.isFavorite = true
-                }
-            }
-        }.resume()
+        completion(true)
     }
     
     func markAsSeen(animalId: Int) {
-        let seenUrl = "\(Constants.backendURL)/animals/\(animalId)/seen"
-        guard let url = URL(string: seenUrl) else {
-            errorMessage = "Invalid URL"
+        guard let selectedAnimal = animalsData.first(where: { animal in animal.id == animalId }) else {
+            print("No animal with the ID: \(animalId)")
             return
         }
         
-        let accessToken = UserDefaultsManager().getAccessToken()
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization") // Add your token
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-//        isLoading = true
-//        isFavorite = true
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-//                self.isLoading = false
-                
-                if let error = error {
-                    self.errorMessage = "Mark as Seen request failed: \(error.localizedDescription)"
-//                    self.isFavorite = false
-//                    completion(false)
-                    return
-                }
-                
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    self.errorMessage = "Invalid response from the server"
-//                    self.isFavorite = false
-//                    completion(false)
-                    return
-                }
-                
-                switch httpResponse.statusCode {
-                case 201:
-//                    completion(true)
-                    print("Animal \(animalId) was marked as seen")
-                default:
-                    self.errorMessage = "Failed with HTTP code: \(httpResponse.statusCode)"
-//                    self.isFavorite = false
-//                    completion(false)
-                }
-            }
-        }.resume()
+        offlineUser.seenAnimals.append(selectedAnimal)
     }
 }
--- natuar/Domains/Shared/Data/OfflineUser.swift
+++ natuar/Domains/Shared/Data/OfflineUser.swift
@@ -19,3 +19,5 @@ struct OfflineUser: Codable, Identifiable {
     var favoriteAnimals: [Animal]
     var seenAnimals: [Animal]
 }
+
+var offlineUser = OfflineUser(user: User(id: 1, name: "Usuario Offline", email: "offline@test.com", created_date: "2024-04-26T05:08:14.170Z", updated_date: "2024-04-26T05:08:14.170Z"), favoriteAnimals: [], seenAnimals: [])
