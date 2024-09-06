084d8382a02d2d874aa101aeab6511bb8a5d9041
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2024-09-06 16:35:46 -0500

    feat: mock data

---
+++ natuar/Domains/Shared/Data/SpotsData.swift
@@ -0,0 +1,234 @@
+//
+//  SpotsData.swift
+//  natuar
+//
+//  Created by Jhonel Rios Jaimes on 6/09/24.
+//
+
+import Foundation
+
+let spotsData: [Spot] = [
+    Spot(
+        id: 4,
+        name: "Parque Nacional del Manu",
+        description: "Ubicado en la provincia de Manu, Madre de Dios, el parque es uno de los parques nacionales más grandes de Perú, hogar de diversas especies endémicas y amenazadas. Es un sitio clave para la conservación y estudio de la Amazonía.",
+        latitude: -12,
+        longitude: -71,
+        picture: "https://s3-us-west-2.amazonaws.com/denomades/blog/wp-content/uploads/2019/01/31141855/peru-manu-atalaya-flip.jpg",
+        images: [
+            "https://th.bing.com/th/id/OIP.zitu2SZW8LJsMuwAljDp9wAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/OIP.MkOOkkhRCsyq7HJIwR9QkgAAAA?rs=1&pid=ImgDetMain"
+        ]
+    ),
+    Spot(
+        id: 1,
+        name: "Machu Picchu",
+        description: "Situado en el distrito de Machupicchu en la provincia de La Convención, es una antigua ciudadela inca ubicada en las montañas de los Andes en Perú. Es uno de los sitios arqueológicos más famosos del mundo y un símbolo de la civilización inca.",
+        latitude: -13.163068,
+        longitude: -72.545128,
+        picture: "https://www.climbing-kilimanjaro.com/wp-content/uploads/2023/03/Machu-Picchu-Elevation.jpg",
+        images: [
+            "https://th.bing.com/th/id/OIP.csidv__lIivseoa1hB6VDwAAAA?rs=1&pid=ImgDetMain",
+            "https://www.freecountrymaps.com/pictures/photo/peru/peru-mountains-11536.jpg",
+            "https://images.roamright.com/uploadedImages/Content_Images/Blog_Photos/2015/09/MPicchuContent.jpg"
+        ]
+    ),
+    Spot(
+        id: 2,
+        name: "Selva Central",
+        description: "Localizada en la provincia de Chanchamayo, Junín, esta área es conocida por su densa vegetación tropical y biodiversidad. Es un destino popular para el ecoturismo y aventuras en la selva.",
+        latitude: -11.12,
+        longitude: -75.35,
+        picture: "https://www.ytuqueplanes.com/imagenes/fotos/novedades/YTQP-Banner-Selva.jpg",
+        images: [
+            "https://th.bing.com/th/id/OIP.ZwRTGxPijaO4Rw6G9JavVgAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/OIP.Iev4MBNrarUXlvIXjY6vrQAAAA?rs=1&pid=ImgDetMain"
+        ]
+    ),
+    Spot(
+        id: 3,
+        name: "Reserva Nacional Tambopata",
+        description: "Esta reserva está situada en la provincia de Tambopata, Madre de Dios, y es reconocida por su rica biodiversidad y extensos bosques tropicales. Es un punto de interés para la observación de aves y fauna silvestre.",
+        latitude: -12.83,
+        longitude: -69.183333,
+        picture: "https://www.lorenzoexpeditions.com/wp-content/uploads/2022/12/RESERVA-TAMBOPATA-1-2-1024x576.jpg",
+        images: [
+            "https://th.bing.com/th/id/OIP.NX9Je8_M2bP0-tNExpMwVwAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/OIP.iXKbZPXWG5Ag7ZWtbU4GrwAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/OIP.3V2lH0T1ck7TIXHdz7jXPAAAAA?rs=1&pid=ImgDetMain"
+        ]
+    ),
+    Spot(
+        id: 11,
+        name: "Reserva Nacional Paracas",
+        description: "Situada en Pisco, Ica, esta reserva protege una diversidad de especies marinas y es famosa por sus formaciones rocosas costeras y las Islas Ballestas.",
+        latitude: -13.834722,
+        longitude: -76.248333,
+        picture: "https://turismoi.pe/uploads/photo/version3/photo_file/72569/optimized_reserva_nacional_de_paracas_1.jpg",
+        images: [
+            "https://www.radionacional.gob.pe/sites/default/files/reserva_paracas.jpg",
+            "https://th.bing.com/th/id/OIP._mZs-5T4Mzj1WoZwjKN2OgAAAA?rs=1&pid=ImgDetMain"
+        ]
+    ),
+    Spot(
+        id: 5,
+        name: "Valle Sagrado",
+        description: "En la provincia de Paucartambo, Cusco, el Valle Sagrado de los Incas es una región rica en sitios arqueológicos, incluyendo Ollantaytambo y Pisac, famosa por sus paisajes andinos y su significado histórico.",
+        latitude: -13.332778,
+        longitude: -72.084444,
+        picture: "https://turismoi.pe/uploads/photo/version3/photo_file/72522/optimized_valle_sagrado_1.jpg",
+        images: [
+            "https://th.bing.com/th/id/R.d1585e5803fab95d192c79a80408d24f?rik=bkt5WhqMUoLnxw&riu=http%3a%2f%2fwww.michaelseeger.de%2freisen%2fpe%2fmachu%2f271.tal2.jpg&ehk=9ys0l51roWT%2fYEr8pV7%2bFdjdfuBa%2bRs3TbMk%2fnaI8t0%3d&risl=&pid=ImgRaw&r=0",
+            "https://th.bing.com/th/id/OIP.Wb5jw39k6eWapqR0rEYQigAAAA?rs=1&pid=ImgDetMain"
+        ]
+    ),
+    Spot(
+        id: 6,
+        name: "Catarata Gocta",
+        description: "Situada en la provincia de Utcubamba, Amazonas, la Catarata Gocta es una de las cascadas más altas del mundo, conocida por su impresionante caída de agua en medio de la selva amazónica.",
+        latitude: -6.007778,
+        longitude: -77.865833,
+        picture: "https://th.bing.com/th/id/R.40bce140c4dcea78de15d7b2db1797fa?rik=TU4Cgys6%2bTrOFw&riu=http%3a%2f%2fblog.redbus.pe%2fwp-content%2fuploads%2f2018%2f04%2fCataratas-del-Gocta-1.jpg&ehk=syT%2bdGpxhIgStds08JTs8YkHF4Bcn2GKnKpGL2tvkX0%3d&risl=&pid=ImgRaw&r=0",
+        images: [
+            "https://i.ytimg.com/vi/wrqIiC_rLMo/hqdefault.jpg",
+            "https://th.bing.com/th/id/OIP.hAnMVZ0j1UEBZpGJFXGc7AAAAA?rs=1&pid=ImgDetMain"
+        ]
+    ),
+    Spot(
+        id: 7,
+        name: "Sillustani",
+        description: "Ubicado cerca de la ciudad de Puno, en la provincia de El Collao, Sillustani es un antiguo cementerio preincaico conocido por sus chullpas, torres funerarias circulares construidas en piedra.",
+        latitude: -15.723056,
+        longitude: -70.158889,
+        picture: "https://th.bing.com/th/id/OIP.HwvYsT0PC80zBkfX61hGvQHaE7?rs=1&pid=ImgDetMain",
+        images: [
+            "https://th.bing.com/th/id/OIP.ENK-COw-m0uRILDZV0dz_gAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/R.c8f8a44c2d8e34431330e3f8d7fe2b65?rik=fj0kutgZCGca%2fg&riu=http%3a%2f%2fperu-tours.com.pe%2fimages%2fsillustani_puno.jpg&ehk=tnCBzgc651jt3dCnz6lN5UqLs3%2fs9c1A2%2bkAyXX0v8I%3d&risl=&pid=ImgRaw&r=0",
+            "https://th.bing.com/th/id/OIP.FDB_S_tnECDrbZkLj_m43QAAAA?rs=1&pid=ImgDetMain"
+        ]
+    ),
+    Spot(
+        id: 8,
+        name: "Valle del Colca",
+        description: "Localizado en la provincia de Caylloma, Arequipa, el Valle del Colca es famoso por su profundo cañón, pintorescos pueblos coloniales y la presencia del majestuoso cóndor andino.",
+        latitude: -15.609329,
+        longitude: -71.857766,
+        picture: "https://blog.redbus.pe/wp-content/uploads/2017/04/shutterstock_439732774.jpg",
+        images: [
+            "https://th.bing.com/th/id/OIP.TfNUQ5Gx7WJLtmCt8-is6gAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/R.4a0d7aa65558d50f64737e74695dc88a?rik=c6P12a7dsRvM2w&riu=http%3a%2f%2fcolca.info%2fimages%2fvalle-del-colca.jpg&ehk=EhDui5gDOquGQCZttoZZWWFlurG5ipULmVvkedPy3Nc%3d&risl=&pid=ImgRaw&r=0",
+            "https://th.bing.com/th/id/R.4f87eadc8d6f3d5572cbd45c9d187de4?rik=DrJfJnpwRjgIBA&pid=ImgRaw&r=0"
+        ]
+    ),
+    Spot(
+        id: 9,
+        name: "Reserva Nacional Pacaya Samiria",
+        description: "En la provincia de Requena, Loreto, esta reserva es conocida como el \"espejo de agua\" por sus bosques inundados y es un destino crucial para la observación de la biodiversidad amazónica.",
+        latitude: -5.5,
+        longitude: -74,
+        picture: "https://www.ytuqueplanes.com/imagenes/fotos/novedades/reserva-nacional-pacaya-samiria-turistas-rio.jpg",
+        images: [
+            "https://th.bing.com/th/id/OIP.LdxOpRwEwGAKLIziw1RBGwAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/OIP.-dapTZ59H-AioVkWx0jLwQAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/OIP.2DsRSN2zqe_d744XzCWp4wAAAA?rs=1&pid=ImgDetMain"
+        ]
+    ),
+    Spot(
+        id: 10,
+        name: "Parque Nacional Cordillera Azul",
+        description: "Localizado en Coronel Portillo, Ucayali, este parque nacional destaca por sus montañas cubiertas de bosques vírgenes y su gran diversidad de especies endémicas.",
+        latitude: -8.25,
+        longitude: -75.5,
+        picture: "https://www.carboncreditcart.com/wp-content/uploads/2021/05/Cordillera-Azul-National-Park-1.jpg",
+        images: [
+            "https://th.bing.com/th/id/OIP.hAeAQ0_H4mp37eCfWGmedwAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/R.a9842309b7333fdec89372d76f6ec1fb?rik=T2j4ZIdInEBNzg&riu=http%3a%2f%2fs3.amazonaws.com%2fimg.inforegion.pe%2fwp-content%2fuploads%2f2010%2f06%2fparque-nacional-cordillera-azul4.jpg&ehk=ktTHf5xm5loCDpwkVca0N66un24XTa%2f02S%2fntzgSeAw%3d&risl=&pid=ImgRaw&r=0"
+        ]
+    ),
+    Spot(
+        id: 12,
+        name: "Selva Alta",
+        description: "En Moyobamba, San Martín, la Selva Alta es conocida por su clima templado y biodiversidad, ideal para el cultivo de orquídeas y la observación de aves.",
+        latitude: -6.033333,
+        longitude: -76.966667,
+        picture: "https://i.pinimg.com/originals/70/bf/0d/70bf0d5c184c18ce3a090f2289193e27.jpg",
+        images: [
+            "https://www.monografias.com/docs110/unidades-geomorfologicas-selva-peruana/image005.jpg",
+            "https://th.bing.com/th/id/OIP.cryWqAXm78aF1IkzLJjV0wAAAA?rs=1&pid=ImgDetMain"
+        ]
+    ),
+    Spot(
+        id: 13,
+        name: "Selva Baja",
+        description: "En la provincia de Bagua, Amazonas, la Selva Baja se caracteriza por su clima cálido y húmedo, siendo un hábitat rico en flora y fauna amazónica.",
+        latitude: -5.65,
+        longitude: -78.45,
+        picture: "https://th.bing.com/th/id/R.ab7299ebc3344796095a100e9268b11b?rik=Ayct928%2fUQ5iRg&pid=ImgRaw&r=0",
+        images: [
+            "https://th.bing.com/th/id/R.fa48e5d518a9565abd24382871bc4ade?rik=Q9DphuHhHFZURA&riu=http%3a%2f%2f2.bp.blogspot.com%2f_2wrL6WcW5aY%2fSw6PkrAL2lI%2fAAAAAAAAAFI%2fSIBEFK01oL0%2fs1600%2funtitled.bmp&ehk=Q%2bkR1GZhgZrFICXwID91KoDoJL4V6mKvf9DKc8hHN4k%3d&risl=&pid=ImgRaw&r=0",
+            "https://www.meteorologiaenred.com/wp-content/uploads/2020/11/region-omagua-452x300.jpg"
+        ]
+    ),
+    Spot(
+        id: 14,
+        name: "Lago Titicaca",
+        description: "Ubicado en Puno, el Lago Titicaca es el lago navegable más alto del mundo y es un lugar central en la mitología andina, compartido entre Perú y Bolivia.",
+        latitude: -15.925,
+        longitude: -69.335,
+        picture: "https://machupicchubrasil.com.br/blog/wp-content/uploads/2019/02/Titicaca-.jpg",
+        images: [
+            "https://th.bing.com/th/id/OIP.JvRK8w5RroTh22OjRJOBHAAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/OIP.DNygvX_py2ap7QPFLvi5FwAAAA?rs=1&pid=ImgDetMain"
+        ]
+    ),
+    Spot(
+        id: 15,
+        name: "Playas de Tumbes",
+        description: "En la provincia de Zarumilla, Tumbes, estas playas son famosas por sus aguas cálidas y biodiversidad marina, incluyendo santuarios de aves y áreas protegidas.",
+        latitude: -3.566944,
+        longitude: -80.451389,
+        picture: "https://turismo.regiontumbes.gob.pe/imagenes/slider-playas-tumbes.jpg",
+        images: [
+            "https://th.bing.com/th/id/R.14a09cb985ceac7f1ae20fd2261c2918?rik=KNKLnIb%2bif8QOA&pid=ImgRaw&r=0&sres=1&sresct=1",
+            "https://www.absolutviajes.com/wp-content/uploads/2011/04/Tumbes4.jpg"
+        ]
+    ),
+    Spot(
+        id: 16,
+        name: "Cordillera Blanca",
+        description: "Ubicada en Huaraz, Ancash, la Cordillera Blanca es la cadena montañosa tropical más alta del mundo y un destino popular para el alpinismo y el ecoturismo.",
+        latitude: -9,
+        longitude: -77,
+        picture: "https://www.peru.travel/Contenido/Atractivo/Imagen/en/141/1.1/Principal/cordillera-blanca.jpg",
+        images: [
+            "https://2.bp.blogspot.com/-Mpl4XEs6GaE/WP5HP8CyTZI/AAAAAAAAADk/fk_e8W4SueMb88LWJcN-cJ0mKcaS4DdvwCLcB/w400-h279/Cordillera_Blanca1.jpg",
+            "https://th.bing.com/th/id/OIP._vXwgMUI2GukRoPXJhCyHQAAAA?rs=1&pid=ImgDetMain",
+            "https://www.fotopaises.com/Fotos-Paises/t/2004/11/10/314_1100080626.jpg"
+        ]
+    ),
+    Spot(
+        id: 17,
+        name: "Reserva Nacional Lomas de Lachay",
+        description: "Situada en Huaral, Lima, esta reserva protege un ecosistema de lomas costeras que florecen en la temporada de nieblas, creando un oasis de biodiversidad en el desierto.",
+        latitude: -11.35,
+        longitude: -77.3,
+        picture: "https://www.matttilghman.com/wp-content/uploads/2017/09/Bay-Area-Foothills-Spring-1024x648.jpg",
+        images: [
+            "https://th.bing.com/th/id/OIP.slmCHzAFVoxIzkafFdxKKgAAAA?rs=1&pid=ImgDetMain",
+            "https://th.bing.com/th/id/OIP.SLtVv_PqIH568jy-dhrcDQAAAA?rs=1&pid=ImgDetMain",
+            "https://2.bp.blogspot.com/-0pf14F4V0lQ/UiFjC-MC0qI/AAAAAAAAApU/GNvRwZyenLg/s1600/LACHAY.jpg"
+        ]
+    ),
+    Spot(
+        id: 18,
+        name: "Zona Reservada Pantanos de Villa",
+        description: "Localizada en Cañete, Lima, esta área protegida es vital para la conservación de aves migratorias y residentes en la costa central de Perú.",
+        latitude: -12.217778,
+        longitude: -76.9375,
+        picture: "https://th.bing.com/th/id/OIP.hSCXhkK6n3lvevt4vUZhsgHaEn?rs=1&pid=ImgDetMain",
+        images: [
+            "https://th.bing.com/th/id/R.9e6c9727ac53e992361823d56393aecb?rik=BIVZ82bqZMUWZw&riu=http%3a%2f%2f3.bp.blogspot.com%2f_H87G81-AiOs%2fTMXwJ0oc8EI%2fAAAAAAAAAJE%2fxUl65QFZ0NU%2fw1200-h630-p-k-no-nu%2fpantanosdevilla.jpg&ehk=lpQ%2foNl1WIBtQoV33jjWOfXYcgm3Qm8LlsB%2b3yV7Kyk%3d&risl=&pid=ImgRaw&r=0",
+            "https://th.bing.com/th/id/R.6422d82e63cb2722a3956c17b3f9ada1?rik=7Gu6IdNBmujarQ&riu=http%3a%2f%2fwww.oocities.org%2fecologia_peru%2fPantanos_de_Villa.jpg&ehk=uyT5R%2foW1XAT4nMpwW%2burLSXLE1aU2NyA74xB2TVpLM%3d&risl=&pid=ImgRaw&r=0"
+        ]
+    )
+]
