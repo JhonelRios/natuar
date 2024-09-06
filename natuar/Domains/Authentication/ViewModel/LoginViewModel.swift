2e9f66b6b5458391da34944282fdd10ad5fd37a4
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2024-09-06 17:11:46 -0500

    refactor: authentication view model

--- natuar/Domains/Authentication/ViewModel/LoginViewModel.swift
+++ natuar/Domains/Authentication/ViewModel/LoginViewModel.swift
@@ -17,45 +17,7 @@ class LoginViewModel : ObservableObject {
     @Published var isLoading: Bool = false
     
     func login(email: String, password: String, completion: @escaping (Bool) -> Void) {
-        let loginURL = "\(Constants.backendURL)/auth/login"
-        guard let url = URL(string: loginURL) else { return }
-        
-        let body = ["email": email, "password": password]
-        guard let jsonBody = try? JSONSerialization.data(withJSONObject: body) else { return }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = jsonBody
-        
-        isLoading = true
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            DispatchQueue.main.async {
-                self.isLoading = false
-                
-                if let error = error {
-                    self.errorMessage = "Login request failed: \(error.localizedDescription)"
-                    completion(false)
-                    return
-                }
-                
-                guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 201 else {
-                    self.errorMessage = "Error: Non-201 HTTP status code"
-                    completion(false)
-                    return
-                }
-                
-                if let loginResponse = try? JSONDecoder().decode(LoginResponse.self, from: data) {
-                    self.accessToken = loginResponse.access_token
-                    UserDefaults.standard.set(loginResponse.access_token, forKey: Constants.accessTokenKey)
-                    completion(true)
-                } else {
-                    self.errorMessage = "Failed to decode login response"
-                    completion(false)
-                }
-            }
-        }.resume()
+        completion(true)
     }
     
     func loginWithGoogle(token: String, completion: @escaping (Bool) -> Void) {
